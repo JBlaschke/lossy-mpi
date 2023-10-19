e3828c0f0c400e58e023112915d226858a89739e
Johannes Blaschke <johannes@blaschke.science>
2023-10-19 13:55:29 +0200

    reorganize tests

--- tests/test_barrier.py
+++ tests/test_barrier.py
@@ -6,65 +6,64 @@ from random import randint
 from sys import argv
 
 
-def run_cli():
+def check_mask(a, b):
+    for x, y in zip(a, b):
+        assert x == y
+
+
+@pytest.mark.mpi(min_size=4)
+def test_timeout_barrier():
     from lossy_mpi.pool import Pool, Status
     from mpi4py import MPI
 
-    verbose = False
-    if len(argv) > 1:
-        if argv[1].strip() == "verbose":
-            verbose = True
-
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
+    size = comm.Get_size()
     root = 0
 
     pool = Pool(comm, root, timeout=2, n_tries=10)
+    pool.advance_transaction_counter(500)
     pool.ready()
 
-    n_data = randint(1, 10)
+    # high-numbered ranks will "drop out" first
+    n_data = size - rank
+    n_iter = 0
+    mask_ref = [Status.READY]*size
 
     while True:
-        # simulate graceful failure: if no more work, drop rank from pool
+        # simulate unexpected failure: if no more work, then stop responding
         # decide on current status (i.e. if current rank has data to send)
-        if n_data <= 0:
-            pool.drop()
+        # assume that rank 0 does not fail
+        if n_data <= 0 and rank != root:
+            break
 
         # update mask -- this is not necessary, but it does speeding things up
         pool.sync_mask()
 
-        # decide to break (root checks if all done) + root: print mask
+        # decide to break (root checks if all done) + root conducts unit test by
+        # checking that the mask is what we expect it to be (at this stage of
+        # test test)
         if rank == root:
-            if verbose:
-                print(pool.mask, flush=True)
+            check_mask(pool.mask, mask_ref)
 
             if pool.done:
                 break
-        else:
-            # if done, break out of loop
-            if pool.status is Status.DONE:
-                break
 
         # Update what we expect the reference mask to be: after each iteration
-        # we drop a random Status.READY rank
+        # we drop the highest Status.READY rank
+        n_iter += 1
+        mask_ref[-n_iter] = Status.TIMEOUT
+
+        # decreate the local counter => this will be used to decide if a rank is
+        # to be timing out from the communicator
         if n_data > 0:
             n_data -= 1
 
-        print(f"{rank=} is at the barrier", flush=True)
         pool.barrier()
+        assert True
 
-    print(f"{rank=} has left the loop", flush=True)
-    last_n_data = comm.gather(n_data, root=root)
-
-    if rank == root:
-        print(f"{last_n_data=}")
-
+    # Ensure that prior tests don't overlap with the next text
     comm.barrier()
-
-
-def check_mask(a, b):
-    for x, y in zip(a, b):
-        assert x == y
 
 
 @pytest.mark.mpi(min_size=4)
@@ -121,8 +120,9 @@ def test_dropout_barrier():
         pool.barrier()
         assert True
 
+    # Ensure that prior tests don't overlap with the next text
     comm.barrier()
 
-
 if __name__ == "__main__":
-    run_cli()
+    test_timeout_barrier()
+    test_dropout_barrier()
--- tests/test_gather.py
+++ tests/test_gather.py
@@ -6,14 +6,18 @@ from random import randint
 from sys import argv
 
 
-def run_cli():
+def check_vec(a, b):
+    for x, y in zip(a, b):
+        assert x == y
+
+
+@pytest.mark.mpi(min_size=4)
+def test_timeout_gather():
+    # from lossy_mpi import getLogger
     from lossy_mpi.pool import Pool, Status
     from mpi4py import MPI
 
-    verbose = False
-    if len(argv) > 1:
-        if argv[1].strip() == "verbose":
-            verbose = True
+    # logger = getLogger(__name__)
 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
@@ -21,58 +25,52 @@ def run_cli():
     root = 0
 
     pool = Pool(comm, root, timeout=2, n_tries=10)
+    pool.advance_transaction_counter(600)
     pool.ready()
 
-    n_data = randint(1, 10)
-    data = None
+    # high-numbered ranks will "drop out" first
+    n_data = size - rank
+    n_iter = 0
+    mask_ref = [Status.READY]*size
 
     while True:
-        # simulate graceful failure: if no more work, drop rank from pool
+        # simulate unexpected failure: if no more work, then stop responding
         # decide on current status (i.e. if current rank has data to send)
-        if n_data <= 0:
-            pool.drop()
+        # assume that rank 0 does not fail
+        if n_data <= 0 and rank != root:
+            break
 
         # update mask -- this is not necessary, but it does speeding things up
         pool.sync_mask()
 
         # decide to break (root checks if all done) + root: print mask
         if rank == root:
-            if verbose:
-                print(pool.mask, flush=True)
+            check_vec(pool.mask, mask_ref)
 
             if pool.done:
                 break
-        else:
-            # if done, break out of loop
-            if pool.status is Status.DONE:
-                break
 
-        # generate data, and update what we expect the reference mask to be:
-        # after each iteration we drop a random Status.READY rank
+        # generate data, and update what we expect the reference mask and data
+        # to be: after each iteration we drop the highest Status.READY rank
+        n_iter += 1
+        mask_ref[-n_iter] = Status.TIMEOUT
+        data = rank + n_iter
+
+        all_data = pool.gather(data)
+        if rank == root:
+            data_ref = [i + n_iter for i in range(size)]
+            for i, m in zip(range(size), pool.mask):
+                if m != Status.READY:
+                    data_ref[i] = None
+            check_vec(all_data, data_ref)
+
+        # decreate the local counter => this will be used to decide if a rank
+        # is to be dropped from the communicator
         if n_data > 0:
-            data = randint(101, 200)
             n_data -= 1
 
-        # communicate data
-        all_data = pool.gather(data)
-
-        # print data
-        if rank == root:
-            print(all_data)
-
-    last_n_data = comm.gather(n_data, root=root)
-    last_data = comm.gather(data, root=root)
-
-    if rank == root:
-        print(f"{last_n_data=}")
-        print(f"{last_data=}")
-
+    # Ensure that prior tests don't overlap with the next text
     comm.barrier()
-
-
-def check_vec(a, b):
-    for x, y in zip(a, b):
-        assert x == y
 
 
 @pytest.mark.mpi(min_size=4)
@@ -138,4 +136,5 @@ def test_dropout_gather():
 
 
 if __name__ == "__main__":
-    run_cli()
+    test_dropout_gather()
+    test_timeout_gather()
